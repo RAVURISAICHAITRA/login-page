72d7cfeb49238b359d26798c7bf696fa4588b32d
Chaitra <ravurisaichaitra@gmail.com>
2022-04-21 16:48:06 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p className='help'><strong>Help ?</strong> </p>
+      <h1 className="main">Accelarate.</h1>
+      <p className="sub__main">UNLEASHING THE BEST IN YOUTH</p>
+      <p className="sign__in">Already have an account with Accelarate? <a> Sign in</a></p>
+      <div className="text__components">
+      <input className='text' type="text" placeholder="Username"/>
+      <input className='text' type="text" placeholder="Email"/>
+      <input className='text' type="text" placeholder="Password"/>
+      <input type="checkbox" />
+        <label className="check"> I agree with <a>Terms and Conditons</a></label>
+      <button className='button'>  SIGN UP</button>
+      </div>
     </div>
   );
 }
